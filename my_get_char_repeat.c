051a503a773e884985f72886e698b6b082666cbc
ferreoljdm <ferreoldemuizon@gmail.com>
2024-06-29 17:24:29 +0000

    my_get_char_repeat

--- my_get_char_repeat.c
+++ my_get_char_repeat.c
@@ -1,3 +1,13 @@
 int my_get_char_repeat(char to_find, const char *to_search){
-    
+    int i = 0;
+    int a = 0;
+    while (to_search[i] != '\0')
+    {
+        if (to_search[i] == to_find)
+        {
+            a++;
+        }
+        i++;
+    }
+    return a;
 }
